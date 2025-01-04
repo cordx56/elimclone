a0d0ca0201ab65b210ef60c974f02cfbbad0a8ad
CHISEN Kaoru <cordx56@cordx.cx>
2025-01-04 09:59:03 +0900

    関数名をコマンドライン引数で受け取る

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,14 @@
 use analycore::rewrite_fn;
 use std::env::args;
 use std::fs::read_to_string;
-use std::io::{self, Write};
 
 fn main() {
     simple_logger::init().unwrap();
 
     let file = args().nth(1).unwrap();
     let source = read_to_string(&file).unwrap();
-    print!("input function name to be rewritten: ");
-    io::stdout().flush().unwrap();
-    let mut fn_name = String::new();
-    io::stdin().read_line(&mut fn_name).unwrap();
-    let res = rewrite_fn(file.into(), source, fn_name.trim());
+    let fn_name = args().nth(2).unwrap();
+    let res = rewrite_fn(file.into(), source, &fn_name);
     if let Ok(res) = res {
         println!("{}", res);
     } else {
